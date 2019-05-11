317f0cf4b3a4f269fd98851d79060ceeaa76e007
rfenzo <rfenzo@uc.cl>
2019-05-11 13:34:32 -0400

    fix: minor locale change

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,8 +25,9 @@ class User < ApplicationRecord
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable, :validatable, :rememberable
-
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+         
   private
 
   def requireValidationField

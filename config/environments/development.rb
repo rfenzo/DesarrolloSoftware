9074dea9f7e5d285d7065dc00af443a3117a92ea
jorgeriesco <jnriesco@uc.cl>
2019-05-11 19:38:48 -0400

    Revert "My dnations"

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,12 +1,10 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
-
-  # Paperclip
-  Paperclip.options[:command_path] = "/usr/bin/"
 
   # Do not eager load code on boot.
   config.eager_load = false
@@ -32,9 +30,6 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.perform_caching = false
-
-  # Devise
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
--- db/migrate/20190505161851_add_details_to_user.rb
+++
@@ -1,8 +0,0 @@
-class AddDetailsToUser < ActiveRecord::Migration[5.1]
-  def change
-    add_column :users, :name, :string
-    add_column :users, :description, :string
-    add_column :users, :rut, :string
-    add_column :users, :address, :string
-  end
-end

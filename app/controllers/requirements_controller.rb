058a9e21e4a7dbaae6cd0843e0246c1f0b7065b6
jorge <jariesco@miuandes.cl>
2019-05-14 16:54:00 -0400

    eliminar solicitudes

--- app/controllers/requirements_controller.rb
+++ app/controllers/requirements_controller.rb
@@ -1,12 +1,12 @@
 class RequirementsController < ApplicationController
+  before_action :requirement_params, only: [:create, :destroy]
+
   def create
     unless isSocialCompany?
       flash[:error] = t(:sign_in, scope: %i[flash require error])
       redirect_to :new_user_session
     else
-      company = User.find(params[:company_id])
-      project = Project.find(params[:project_id])
-      requirement = Requirement.new({project: project, user: company})
+      requirement = Requirement.new(requirement_params)
       if requirement.save
         flash[:success] = t(:default, scope: %i[flash require success], project: project.name, company: company.name)
         redirect_to my_social_projects_path
@@ -22,10 +22,16 @@ class RequirementsController < ApplicationController
       flash[:error] = t(:sign_in, scope: %i[flash require error])
       redirect_to :new_user_session
     else
-      requirement = Requirement.where({project_id: params[:project_id], user: params[:company_id]}).destroy
+      requirement = Requirement.find_by(requirement_params)
       requirement.destroy
-      flash[:success] = t(:destroy, scope: %i[flash requirement success], requirement: requirement.project.name)
+      flash[:success] = t(:destroy, scope: %i[flash require success], project: requirement.project.name)
       redirect_to :my_requirements
     end
+  end
+
+  private
+
+  def requirement_params
+    params.require(:requirement).permit(:project_id, :company_id)
   end
 end

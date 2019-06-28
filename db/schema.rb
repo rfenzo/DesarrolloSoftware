2d2068f1d60867874d2950ac539afb7b7e940d20
rfenzo <rfenzo@uc.cl>
2019-06-28 13:35:40 -0400

    ignore dotfiles and shcema update

--- db/schema.rb
+++ db/schema.rb
@@ -37,7 +37,6 @@ ActiveRecord::Schema.define(version: 20190510201825) do
   create_table "projects", force: :cascade do |t|
     t.string "name"
     t.text "description"
-    t.integer "amount", default: 0
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false

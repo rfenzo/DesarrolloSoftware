a31ea778142bbdbab3b371d6cf2edccc03ff4db2
jorgeriesco <jnriesco@uc.cl>
2019-06-30 22:22:40 -0400

    landing

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,5 +4,10 @@ class HomeController < ApplicationController
   before_action :set_ranking_variables
 
   def landing
+    projects = Project.all
+    projects.map(&:calculate_donations)
+    @most_donated = projects.max_by(&:total_donations)
+    @recent_projects = projects.order(:created_at).first(3)
+    @most_sponsored = Contract.group(:project).count.max[0]
   end
 end

05f3350566b2bceb53bfa531f89910e4a3ae8dd4
= <dagalloso@uc.cl>
2019-07-01 17:41:10 -0400

    trending

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -9,6 +9,7 @@ class HomeController < ApplicationController
     @random_project = projects.sample
     @most_donated = projects.max_by(&:total_donations)
     @recent_projects = projects.order(:created_at).first(3)
+    @recent_projects.map(&:calculate_donations)
     @most_sponsored = Contract.group(:project).count.max[0]
   end
 end
